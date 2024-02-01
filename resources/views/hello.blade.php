a841375082a4092a931bbd4fe77b1f4b90af386f
KobraDent <mr.vladimir663@gmail.com>
2024-02-01 22:12:05 +0500

    initial

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>609-11</title>
+</head>
+<body>
+    <h2>{{ $title }}</h2>
+</body>
+</html>
